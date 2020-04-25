a997c2e3b024cbfe59a16e7e5b323b51dab92c42
Ratan Rai Sur <ratan.r.sur@gmail.com>
2020-04-25 11:18:28 -0400

    points and midpoints
    Signed-off-by: Ratan Rai Sur <ratan.r.sur@gmail.com>

--- src/Main.elm
+++ src/Main.elm
@@ -1,29 +1,48 @@
 module Main exposing (main)
 
 import Html exposing (Html)
-import TypedSvg exposing (polygon, svg)
-import TypedSvg.Attributes exposing (class, height, id, points, width)
-import TypedSvg.Types exposing (Length(..), pc, pt)
+import TypedSvg exposing (circle, svg)
+import TypedSvg.Attributes exposing (cx, cy, height, r, width)
+import TypedSvg.Core exposing (Svg)
+import TypedSvg.Types exposing (Length(..), pc, px)
+
+
+type alias Point =
+    { x : Float
+    , y : Float
+    }
+
+
+pointA : Point
+pointA =
+    { x = 20, y = 20 }
+
+
+pointB : Point
+pointB =
+    { x = 450, y = 450 }
+
+
+midPoint : Point -> Point -> Point
+midPoint a b =
+    { x = (a.x + b.x) / 2, y = (a.y + b.y) / 2 }
+
+
+myCircle : Point -> List (Html.Attribute msg) -> List (Svg msg) -> Html.Html msg
+myCircle center attrs =
+    circle ([ cx (px center.x), cy (px center.y), r (px 5) ] ++ attrs)
 
 
 main : Html a
 main =
-    svg
-        [ id "color-fill", width (pc 100), height (pt 300) ]
-        [ polygon
-            [ class [ "hex" ], points [ ( 300, 150 ), ( 225, 280 ), ( 75, 280 ), ( 0, 150 ), ( 75, 20 ), ( 225, 20 ) ] ]
+    svg [ height (pc 100), width (pc 100) ]
+        [ myCircle pointB
+            []
+            []
+        , myCircle pointA
+            []
+            []
+        , myCircle (midPoint pointA pointB)
+            []
             []
         ]
-
-
-hexagonPoints : ( Int, Int ) -> Int -> List ( Float, Float )
-hexagonPoints (x, y) sideLength =
-    direction = 0 --degrees
-    let firstPoint = (x-sideLength/2, y + sideLength/(2*(tan pi/2))
-    --[(x-sideLength/2,)
-    --,(x+sideLength/2,)
-    --,(,)
-    --,(,)
-    --,(x-sideLength/2,)
-    --,(,)]
-
