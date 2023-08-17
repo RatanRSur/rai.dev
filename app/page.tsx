cb5a1a862ab2cc9edc723e83f7c74e239a1fc112
Rai <ratan.r.sur@gmail.com>
2023-08-17 10:37:03 -0400

    more nav items

--- app/page.tsx
+++ app/page.tsx
@@ -94,9 +94,22 @@ export default function Home() {
         <div class="navigation">
           <p>rai</p>
           <p>
+            <Link href="/about">
+              <u>about</u>
+            </Link>
+          </p>
+          <p>
             <Link href="/services">
               <u>services</u>
             </Link>
+          </p>
+          <p>
+            <Link href="/testimonials">
+              <u>testimonials</u>
+            </Link>
+          </p>
+          <p>
+            <u>contact</u>
           </p>
         </div>
       </div>
