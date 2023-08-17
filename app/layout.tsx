4e9f5820790e02564b61a8eb61046e8145cc0cfb
Rai <ratan.r.sur@gmail.com>
2023-08-17 16:41:16 -0400

    fix window issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,27 +22,20 @@ function generateRandomPointsNormalized(
 const normalizedInitialPoints = generateRandomPointsNormalized(2 ** 5);
 const radii = normalizedInitialPoints.map(() => Math.random() * 50 + 50);
 
-function useWindowSize(): { width: number; height: number } {
-  // Initialize state with undefined width/height so server and client renders match
-  // Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
-  const [windowSize, setWindowSize] = useState({
-    width: window.innerWidth,
-    height: window.innerHeight,
-  });
+function useWindowSize() {
+  const [windowSize, setWindowSize] = useState({ width: 0, height: 0 });
+
   useEffect(() => {
-    // Handler to call on window resize
-    function handleResize() {
-      // Set window width/height to state
-      setWindowSize({
-        width: window.innerWidth,
-        height: window.innerHeight,
-      });
+    if (typeof window !== "undefined") {
+      const handleResize = () => {
+        setWindowSize({ width: window.innerWidth, height: window.innerHeight });
+      };
+      window.addEventListener("resize", handleResize);
+      handleResize();
+      return () => window.removeEventListener("resize", handleResize);
     }
-    // Add event listener
-    window.addEventListener("resize", handleResize);
-    // Remove event listener on cleanup
-    return () => window.removeEventListener("resize", handleResize);
-  }, []); // Empty array ensures that effect is only run on mount
+  }, []);
+
   return windowSize;
 }
 
