dee754e0ecda356cc2c52239f157e6ded212eda3
Rai <ratan.r.sur@gmail.com>
2023-08-31 13:47:26 -0400

    add farcaster

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,24 +1,27 @@
+import React from "react";
+
 export default function Contact() {
+  let items = [
+    <a target="_blank" href="https://x.com/0xRaino">
+      x/twitter
+    </a>,
+    <a target="_blank" href="https://warpcast.com/rai">
+      farcaster
+    </a>,
+    <a
+      target="_blank"
+      href="mailto:rai@rai.dev?subject=interested in <topic/service>"
+    >
+      email
+    </a>,
+    <a href="..">⮐</a>,
+  ];
   return (
     <div className="centered-window">
       <div className="normal-text">
-        <p>
-          <a target="_blank" href="https://x.com/0xRaino">
-            x/twitter
-          </a>
-        </p>
-        <p>
-          <a
-            target="_blank"
-            href="mailto:rai@rai.dev?subject=interested in <topic/service>"
-          >
-            email
-          </a>
-        </p>
-        <br></br>
-        <p>
-          <a href="..">⮐</a>
-        </p>
+        {items.map((item, index) => (
+          <p>{item}</p>
+        ))}
       </div>
     </div>
   );
