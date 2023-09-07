d0daaef6a036b92a6bead6e0cc2243d3f44f55fe
Rai <ratan.r.sur@gmail.com>
2023-09-07 15:14:43 -0400

    add keys :/

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -2,25 +2,20 @@ import React from "react";
 
 export default function Contact() {
   let items = [
-    <a target="_blank" href="https://x.com/0xRaino">
-      x/twitter
-    </a>,
-    <a target="_blank" href="https://warpcast.com/rai">
-      farcaster
-    </a>,
-    <a
-      target="_blank"
-      href="mailto:rai@rai.dev?subject=interested in <topic/service>"
-    >
-      email
-    </a>,
-    <a href="..">⮐</a>,
+    ["x/twitter", "https://x.com/0xRaino"],
+    ["farcaster", "https://warpcast.com/rai"],
+    ["email", "mailto:rai@rai.dev?subject=interested in <topic/service>"],
+    ["⮐", ".."],
   ];
   return (
     <div className="centered-window">
       <div className="normal-text">
         {items.map((item, index) => (
-          <p>{item}</p>
+          <p key={index}>
+            <a target="_blank" href={item[1]}>
+              {item[0]}
+            </a>
+          </p>
         ))}
       </div>
     </div>
--- app/writing/page.tsx
+++ app/writing/page.tsx
@@ -5,14 +5,19 @@ export default function Writing() {
     <a
       target="_blank"
       href="https://www.lesswrong.com/posts/zysbfcBfBG67pxbaA/spirits-vs-terms-as-arbitration-norms"
+      key="bets"
     >
       a cautionary tale about how to resolve friendly bets
     </a>,
-    <a target="_blank" href="https://alongside.xyz/blog/crypto-indexes">
+    <a
+      target="_blank"
+      href="https://alongside.xyz/blog/crypto-indexes"
+      key="index-tokens"
+    >
       a primer on the tradeoffs between the technical architectures of index
       tokens
     </a>,
-    <>
+    <React.Fragment key="4444">
       <a
         target="_blank"
         href="https://twitter.com/0xRaino/status/1481657327283359749?s=20"
@@ -23,15 +28,18 @@ export default function Writing() {
       <a target="_blank" href="https://eips.ethereum.org/EIPS/eip-4444">
         eip-4444: bound historical data in execution clients
       </a>
-    </>,
+    </React.Fragment>,
     <a
       target="_blank"
       href="https://giddy-daisy-03d.notion.site/You-Have-the-Wrong-Challenge-45e07366a9004b47a3229b4e249decd9?pvs=4"
+      key="men"
     >
       a common trap many men fall into when faced with their feelings of
       romantic unworthiness
     </a>,
-    <a href="..">⮐</a>,
+    <a href=".." key="back">
+      ⮐
+    </a>,
   ];
 
   return (
