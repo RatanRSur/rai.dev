6e8c11c27097a5cf29d85b8bcc672606dd4f3a6b
Rai <ratan.r.sur@gmail.com>
2025-03-31 12:01:06 +0100

    remove instagram

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -4,7 +4,7 @@ import Back from "../components/Back";
 export default function Contact() {
   let items = [
     ["x/twitter", "https://x.com/rai_sur11"],
-    ["instagram", "https://instagram.com/anditsfunagain"],
+    // ["instagram", "https://instagram.com/anditsfunagain"],
     ["email", "mailto:rai@rai.dev?subject=interested in <topic/service>"],
   ];
   return (
