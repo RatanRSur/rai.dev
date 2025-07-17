08b44ebe6d16dc655000c65217ca83dfdcffe23f
Rai <ratan.r.sur@gmail.com>
2025-07-16 22:10:01 -0500

    update to node 22

--- app/bet/page.tsx
+++ app/bet/page.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from 'react'
 import BettingState from "../components/BettingState";
 import Back from "../components/Back";
 
@@ -5,7 +6,9 @@ export default function Bet() {
   return (
     <div className="h-full flex flex-row justify-center items-center">
       <div className="font-mono p-3">
-        <BettingState />
+        <Suspense>
+          <BettingState />
+        </Suspense>
         <br />
         <br />
         <Back></Back>
