965cc53c910e61be2cc4bca446e9116344cdcd0e
Rai <ratan.r.sur@gmail.com>
2025-08-15 15:52:42 -0400

    Update About page content and add feedback option
    - Add a new section for anonymous feedback.
    - Refine Sentinel's mission statement and past role descriptions.
    - Update the link for existential risk from AI.
    - Enhance the description of personal interests.
    - Remove the mention of Crocker's Rules.
    - Adjust the call booking offer to include a nominal fee.

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -14,8 +14,13 @@ export default function About() {
   return (
     <div className="h-full flex flex-row justify-center items-center">
       <div className="full normal-text p-3">
+        <p> 
+          most important thing up front: i want your honest feedback on anything! it could be something you think i could improve on, a description of what the experience of interacting with me is like, or anything else. to give me feedback anonymously, click <a href="https://www.admonymous.co/rai">here</a>.
+        </p>
+        <br />
+        <br />
         <p>
-          i'm a cofounder @ <a target="_blank" href="https://sentinel-team.org/">sentinel</a>, an org that attempts to foresee large-scale/existential catastrophes and deploy emergency response teams to avert disaster. please <a href="/contact">reach out</a> if you want to learn more, be involved, or support in any way.
+          currently, i help run <a target="_blank" href="https://sentinel-team.org/">sentinel</a>, an org that attempts to foresee large-scale/existential catastrophes and take high leverage actions during crises. please <a href="/contact">reach out</a> if you want to learn more, be involved, or support in any way.
         </p>
         <br />
         <br />
@@ -23,7 +28,7 @@ export default function About() {
           i'm particularly concerned about{" "}
           <a
             target="_blank"
-            href="https://80000hours.org/problem-profiles/artificial-intelligence/"
+            href="https://www.amazon.com/dp/0316595640/"
           >
             existential risk from unaligned artificial intelligence
           </a>
@@ -32,18 +37,18 @@ export default function About() {
         <br />
         <br />
         <p>
-          previously, i've worked as an ai engineer @ microsoft, an{" "}
+          previously, i've worked as an ai engineer for microsoft, an{" "}
           <a target="_blank" href="https://ethereum.org/gl/governance/">
             ethereum core developer
           </a>
-          , a blockchain protocol engineer @{" "}
+          , a blockchain protocol engineer at{" "}
           <a
             target="_blank"
             href="https://consensys.io/products#for-the-ecosystem"
           >
             consensys
           </a>
-          , and cofounder/cto @{" "}
+          , and cofounder/cto of{" "}
           <a target="_blank" href="https://blockworks.co/news/universal-raise-led-by-a16z">
             universal
           </a>
@@ -69,18 +74,13 @@ export default function About() {
         <br></br>
         <br></br>
         <p>
-          i'm a <a target="_blank" href="https://en.wikipedia.org/wiki/Transhumanism">transhumanist</a> who enjoys <a target="_blank" href="https://www.relateful.com/">circling</a>, <a target="_blank" href="https://www.lesswrong.com/posts/yeADMcScw8EW9yxpH/a-sketch-of-good-communication">sharing beliefs productively</a>,  betting on those beliefs, skillfully relating to the sensations in my body, and endless self-improvement.
+          i'm a <a target="_blank" href="https://en.wikipedia.org/wiki/Transhumanism">transhumanist</a> who enjoys <a target="_blank" href="https://www.relateful.com/">circling</a>, <a target="_blank" href="https://www.lesswrong.com/posts/yeADMcScw8EW9yxpH/a-sketch-of-good-communication">sharing beliefs productively</a>, betting on those beliefs, skillfully relating to the sensations in my body (especially through dance and breath), and endless self-improvement.
         </p>
         <br />
         <br />
         <p>
-          i follow <a target="_blank" href="http://sl4.org/crocker.html">crocker's rules</a>.
-        </p>
-        <br />
-        <br />
-        <p>
-          if you want some free advice on anything (personal or professional), you can{" "}
-          <a data-cal-namespace="consultation" data-cal-link="raisur/consultation" data-cal-config='{"layout":"week_view"}'>book a call with me</a>.
+          if you want some advice on anything (personal or professional), you can{" "}
+          <a data-cal-namespace="consultation" data-cal-link="raisur/consultation" data-cal-config='{"layout":"week_view"}'>book a call with me</a> for $1.
         </p>
         <br />
         <br />
